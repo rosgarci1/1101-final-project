81a32bef9f9b4e36c52fbbeba7076e3795d3ab28
Rosie <rosa.garcia07@utrgv.edu>
2023-11-29 16:33:57 -0600

    Added Front-End Code

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-# First Commit
-print("Hello World")
+# Import libraries and packages, they contain additional functions.
+from flask import Flask, render_template
+# Create web application using Flask
+app = Flask(__name__)
+# Create our routes
+@app.route('/')
+def home():
+ return render_template('index.html')
+# Start the web application
+app.run(
+ debug=True
+)
